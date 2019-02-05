b76b262fc6e6bf8fa8f3d4707b876438a3987f4a
irsol <irinywka@gmail.com>
2019-02-05 16:13:43 +0100

    Short solutions to range functions.

--- Python-Bootcamp3/range.py
+++ Python-Bootcamp3/range.py
@@ -1,6 +1,20 @@
+# Print odd numbers from 10 to 20 inclusive and store the result in the variable.
+
 x = 0
 
 for n in range(10, 21):
     if n % 2 != 0:
         x += n
 print(n)
+
+# This solution loops over the odd numbers. Print odd numbers from 10 to 20 inclusive.
+
+x = 0
+
+for i in range(11, 21, 2):
+    x += i
+print(i)
+
+
+
+
