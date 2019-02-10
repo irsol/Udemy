9a9ee493cac805469c7a6fd28925557b549792f7
irsol <irinywka@gmail.com>
2019-02-10 16:41:07 +0100

    Improved while loop task.

--- Python-Bootcamp3/while_loop.py
+++ Python-Bootcamp3/while_loop.py
@@ -1,10 +1,9 @@
 from random import randint  # use randint(a, b) to generate a random number between a and b
 
 number = 0  # store random number in here, each time through
-i = 0  # i should be incremented by one each iteration
-
+i = 0  # i should be incremented by one each iteration, i counts loops till while condition becomes True!
 
 while number != 5:
     i += 1
     number = randint(1, 10)
-    print(number)
+    print(number, i)
