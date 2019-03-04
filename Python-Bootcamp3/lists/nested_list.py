a2ca3252c1c2e831d9f753b8fd0bc85dc35307a8
irsol <irinywka@gmail.com>
2019-03-04 15:03:29 +0100

    Updated.

--- Python-Bootcamp3/lists/nested_list.py
+++ Python-Bootcamp3/lists/nested_list.py
@@ -12,18 +12,23 @@ for l in nested_list:
 nested_list = [[12, 22, 32], [41, 51, 61], [73, 83, 93]]
 
 [[print(val) for val in l] for l in nested_list]
-
 print(nested_list)
 
 # Generate a nested list that can represent a game board
 
 board = [[num for num in range(1, 4)] for val in range(1, 4)]
-
 print(board)
 
 
 # Even = X, Odd = O
 
 board_odd_even = [["X" if num % 2 != 0 else "O" for num in range(1, 4)] for val in range(1, 4)]
-
 print(board_odd_even)
+
+
+# Using a nested list comprehension and range() create a variable called answer with value [0, 1, 2]
+
+answer = [[num for num in range(0, 3)] for val in range(0, 3)]
+print(answer)
+
+#
