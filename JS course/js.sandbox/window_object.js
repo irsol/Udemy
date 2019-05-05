756c65ac57cfa3254c4043c7c0ae653ae68ba766
irsol <irinywka@gmail.com>
2019-05-05 19:12:02 +0200

    Added how to use window objects.

--- JS course/js.sandbox/window_object.js
+++ JS course/js.sandbox/window_object.js
@@ -15,24 +15,24 @@
 // }
 
 // 
-let val;
+// let val;
 // Outer height and width
 // val = window.outerHeight;
 // val = window.outerWidth;
 
 // Inner height and width
-val = window.innerHeight;
-val = window.innerWidth;
+// val = window.innerHeight;
+// val = window.innerWidth;
 
 // Scroll properties
 //val = window.scrollY;
 
 // Location Object
-val = window.location;
-val = window.location.hostname;
-val = window.location.port;
-val = window.location.href;
-val = window.location.search;
+// val = window.location;
+// val = window.location.hostname;
+// val = window.location.port;
+// val = window.location.href;
+// val = window.location.search;
 
 // Redirect
 //window.location.href = "http://google.com";
@@ -44,8 +44,31 @@ val = window.location.search;
 //window.history.lenght;
 
 // Navigator Object works with browser
-val = window.navigator;
-val = window.navigator.appName;
-val = window.navigator.appVersion;
+// val = window.navigator;
+// val = window.navigator.appName;
+// val = window.navigator.appVersion;
+
+// console.log(val);
+
+// Examing the document object
+val = document;
+val = document.all;
+val = document.all[2];
+val = document.body;
+val = document.characterSet;
+val = document.contentType;
+
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+
+val = document.links;
+val = document.links[0].className;
+val = document.links[0].classList[0];
+
+val = document.images;
+val = document.scripts;
+val = document.scripts[1].getAnimations('src');
 
 console.log(val);
