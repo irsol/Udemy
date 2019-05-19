6c4c50e9d7622821b09c578b4e74ac8824a16438
irsol <irinywka@gmail.com>
2019-05-19 18:54:41 +0200

    Updated.

---
+++ JS course/js.sandbox/single_elem_selectors.js
@@ -0,0 +1,38 @@
+// document.getElementById()
+
+// console.log(document.getElementById('task-title'));
+
+// // Get things from the element
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
+
+// const taskTitle = document.getElementById('task-title');
+
+// // Change styling
+// taskTitle.style.background = 'red';
+// taskTitle.style.padding = '5px';
+// // taskTitle.display = 'none';
+
+// // Change content
+// taskTitle.textContent = 'Task list';
+// taskTitle.innerText = 'Your list';
+// taskTitle.innerHTML = '<span style="color:blue">Your list</span>';
+
+
+// // document.querySelector()
+// console.log(document.querySelector('#task-title'));
+// console.log(document.querySelector('.card-title'));
+// console.log(document.querySelector('h5'));
+
+// document.querySelector('li').style.color = 'red';
+// document.querySelector('ul li').style.color = 'blue';
+
+// document.querySelector('li:last-child').style.color = 'red';
+// document.querySelector('li:nth-child(3)').style.color = 'yellow';
+// document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+// document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+// document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
+
+
+
+
